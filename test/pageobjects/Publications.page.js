c895ffcb29aa928533971744ad819fbe08210ad3
amekhedka <alena.mekhedka@gmail.com>
2021-12-21 00:11:07 -0500

    Edit selectors

--- test/pageobjects/Publications.page.js
+++ test/pageobjects/Publications.page.js
@@ -1,11 +1,14 @@
 const Page = require ("./Page");
 
-class PublicationsPage extends Page {
+    class PublicationsPage extends Page {
+        get publicationsTitle() {
+            return $("//h6[text()='publications']")
 
-
+        }
 
     open() {
         return super.open('/publications');
     }
 }
+
 module.exports = new PublicationsPage();
--- test/specs/Login.js
+++ test/specs/Login.js
@@ -4,13 +4,12 @@ const PublicationsPage = require ("../pageobjects/Publications.page")
 describe('My Login application', () => {
 
     it ('should login with valid credentials',  async () => {
-        await LoginPage.open();
         await LoginPage.fillLoginData ('Manya111@test.com', 'Manya111@');
         await LoginPage.clickLoginBtn();
     });
 
     it ("Should redirect on Publication Page", async () => {
-        await expect (PublicationsPage.h6).toBeExisting().true;
-        //await expect (PublicationsPage.h6).toHaveTextContaining("Publications");
+        await expect(PublicationsPage.publicationsTitle).toHaveText("publications");
+        await expect(PublicationsPage.publicationsTitle).toBeExisting().true
     });
 });
