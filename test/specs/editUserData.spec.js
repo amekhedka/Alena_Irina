3bb6e766f3c6198b12d04d99e86e456e2bfd6521
iryna.kalynychenko <irzhik1975@gmail.com>
2021-12-21 08:58:44 -0500

    browser.maximizeWindow()

--- test/specs/editUserData.spec.js
+++ test/specs/editUserData.spec.js
@@ -3,6 +3,10 @@ const PublicationsPage = require ("../pageobjects/Publications.page")
 
 describe('My Login application', () => {
 
+    before(() => {
+        browser.maximizeWindow();
+    })
+
     it ('should login with valid credentials',  async () => {
         await LoginPage.open();
         await LoginPage.fillLoginData ('Manya111@test.com', 'Manya111@');
