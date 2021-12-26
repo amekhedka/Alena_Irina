de14ce7ed7e68ada34638f565b34c339a6bb8fe4
amekhedka <alena.mekhedka@gmail.com>
2021-12-26 16:50:08 -0500

    create test "findProblems"

--- test/specs/problemTest.spec.js
+++ test/specs/problemTest.spec.js
@@ -1,4 +1,6 @@
-const ProblemPage = require('../pageobjects/Company.page')
+const ProblemPage = require('../pageobjects/Problem.page')
+const LoginPage = require('../pageobjects/Login.page')
+const GlobalNavigation = require('../pageobjects/GlobalNavigation.page')
 
 describe('Problem functionality', () => {
 
@@ -6,8 +8,23 @@ describe('Problem functionality', () => {
         browser.maximizeWindow();
     });
 
+    it('Login and open problems Page', async () => {
+        await LoginPage.fillLoginCredentials("Manya111@test.com", "Manya111@");
+        await LoginPage.btnLogIn.click();
+        await GlobalNavigation.btnMenu.click();
+        await GlobalNavigation.problemsOption.click()
+        await expect(ProblemPage.problemTitlePage).toHaveTextContaining("problems")
+    });
 
-    it('Test', async () => {
-
+    it('Search company by name Google', async () => {
+        await ProblemPage.btnFilter.click();
+        await ProblemPage.filterColumnDropDownMenu.selectByVisibleText("Company");
+       // await ProblemPage.filterOperatorDropDownMenu.selectByVisibleText("starts with");
+        await ProblemPage.filterValueDropDown.setValue("Google")
+        const loader = ProblemPage.btnLoad;
+        await expect(loader).not.toBeDisabled(); //типо ждем когда перестанет грузится
+        await ProblemPage.btnFilter.click();
+        const countRowProblems = await ProblemPage.problemRowsContainTextColumn("Google", "Company")
+        await expect(countRowProblems.length).toEqual(10)
     });
 });
