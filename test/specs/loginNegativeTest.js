c6aabc37f3979e578597ba41091f7dccd62fdbcf
amekhedka <alena.mekhedka@gmail.com>
2021-12-25 20:48:52 -0500

    leave questions

--- test/specs/loginNegativeTest.js
+++ test/specs/loginNegativeTest.js
@@ -11,37 +11,44 @@ describe('Login functionality', () => {
         browser.maximizeWindow();
     });
 
-    // it('Should not login with invalid email', async () => {
-    //     await LoginPage.open();
-    //     await LoginPage.inputEmail.setValue('12345678909876543');
-    //     await LoginPage.inputPassword.setValue('Manya111@');
-    //     await LoginPage.btnLogIn.click();
-    //     const res = await LoginPage.errorMessage.getText();
-    //     await expect(res).toEqual("User with provided email does not exist");
-    // });
+    it('Should not login with invalid email', async () => {
+        await LoginPage.open();
+        await LoginPage.inputEmail.setValue('12345678909876543');
+        await LoginPage.inputPassword.setValue('Manya111@');
+        await LoginPage.btnLogIn.click();
+        const res = await LoginPage.errorMessage.getText();
+        await expect(res).toEqual("User with provided email does not exist");
+    });
 
-    //Todo: finished
-    it('shouldn`t login with empty fields', async () => {
-            await LoginPage.fillLoginCredentials('bbbbbbbbbb', 'vvvvvvvvvv');
-            await isObjectEmpty(LoginPage.inputEmail);
-            await isObjectEmpty(LoginPage.inputPassword);
-            await LoginPage.btnLogIn.click();
-            //
-        });
+    it('Enter valid email', async () => {
+        await clearInput(await LoginPage.inputEmail)
+        await LoginPage.inputEmail.setValue('Manya111@test.com');
+        await LoginPage.btnLogIn.click();
+        await expect(PublicationsPage.publicationsTitle).toEqual('publications');
+    });
 
-    //Todo: finished
-    // it('shouldn`t login with empty Email', async () => {
-    //         await LoginPage.fillLoginCredentials('', 'Manya111@');
-    //         await LoginPage.btnLogIn.click();;
+    // //Todo: finished
+    // it('shouldn`t login with empty fields', async () => {
+    //         await LoginPage.fillLoginCredentials('bbbbbbbbbb', 'vvvvvvvvvv');
+    //         await isObjectEmpty(LoginPage.inputEmail);
+    //         await isObjectEmpty(LoginPage.inputPassword);
+    //         await LoginPage.btnLogIn.click();
+    //         //
     //     });
 
     //Todo: finished
+    it('shouldn`t login with empty Email', async () => {
+            await LoginPage.fillLoginCredentials('', 'Manya111@');
+            await LoginPage.btnLogIn.click();;
+        });
+    //
+    // Todo: finished
     // it('shouldn`t login with empty PW', async () => {
     //         await LoginPage.fillLoginCredentials('Manya111@test.com', '');
     //         await LoginPage.btnLogIn.click();
     //     });
-
-    //Todo: finished
+    //
+    // Todo: finished
     // it('Login after refresh browser page', async () => {
     //         await LoginPage.fillLoginCredentials('Manya111@test.com', 'Manya111@');
     //         await browser.refresh();
