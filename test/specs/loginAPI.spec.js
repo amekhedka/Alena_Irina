1ab07605193153baffb24db7a8b47dff43cf3902
amekhedka <alena.mekhedka@gmail.com>
2021-12-26 00:16:17 -0500

    API test

--- test/specs/loginAPI.spec.js
+++ test/specs/loginAPI.spec.js
@@ -1,12 +1,19 @@
 const LoginPage = require('../pageobjects/Login.page');
-const { createUser, req_Link } = require("../../helpers/axios.methods")
+const { registerActivationLink } = require('../../helpers/axios.methods');
+const { createUser } = require('../../helpers/axios.methods');
 
 describe('API for Login page', () => {
-    let result = null;
+    let result = null;   //создали переиенную пока с значением NULL
 
     it('API registration', async () => {
-        result = await createUser('Manya111@test.com', 'Manya111@')
+        result = await createUser('Ma4546n45565678ya111@test.com', 'Manya111@')
         console.log(result)
-        expect(!!result.Activatiom)
+        expect(!!result.actovationLinkId).toBe(true)
+    });
+
+    it('API - user activation', async () => {
+        result = await registerActivationLink(result.actovationLinkId)
+        console.log(result)
+        expect(result.activationString).toHaveText("Activation Successful!")
     });
 });
